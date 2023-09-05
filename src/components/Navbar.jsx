9616ab95a1181fe265629b384cf9ec515cd48d81
Tomas Alvarez <tomasralvarez@gmail.com>
2023-09-05 19:53:15 -0300

    Correccion de navbar y animacion del carrito

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,12 @@ import CartWidget from "./CartWidget";
 
 const Nabvar = () => {
 	return (
-		<div className="bg-body-secondary">
-			<nav className="navbar navbar-expand-lg">
-				<div className="container-fluid navBarDisplay">
+		<div className="header">
+			<nav className="navbar navbar-expand-lg navBarDisplay">
+				<div className="container-fluid">
+					<button className="navbar-toggler text-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
 					<div className="navbarTitle">
 						<a className="navbar-brand fs-2" href="#">
 							<img className="nikeLogo" src={nikeLogo} alt="" />
@@ -14,27 +17,24 @@ const Nabvar = () => {
 							<img className="nikeLogo" src={nikeLogo} alt="" />
 						</a>
 					</div>
-					<button className="navbar-toggler text-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</button>
 					<div className="collapse navbar-collapse w-100 justify-content-center" id="navbarNav">
 						<ul className="navbar-nav navbarList">
-							<li className="nav-item">
-								<a className="nav-link active fs-4" aria-current="page" href="#">
+							<li className="nav-item activeLink">
+								<a className="nav-link fs-4 active" href="#">
 									Inicio
 								</a>
 							</li>
-							<li className="nav-item">
+							<li className="nav-item navItem">
 								<a className="nav-link fs-4" href="#">
 									Hombre
 								</a>
 							</li>
-							<li className="nav-item">
+							<li className="nav-item navItem">
 								<a className="nav-link fs-4" href="#">
 									Mujer
 								</a>
 							</li>
-							<li className="nav-item">
+							<li className="nav-item navItem">
 								<a className="nav-link fs-4" href="#">
 									Ver Todo
 								</a>
@@ -42,8 +42,8 @@ const Nabvar = () => {
 						</ul>
 					</div>
 				</div>
-				<CartWidget />
 			</nav>
+			<CartWidget />
 		</div>
 	);
 };
