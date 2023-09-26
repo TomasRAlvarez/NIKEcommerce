2a1eb3a4c48924741d7d60d82dea1f35b997a033
Tomas Alvarez <tomasralvarez@gmail.com>
2023-09-26 19:52:30 -0300

    Estilado de la pagina de detalles del producto

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,13 +1,45 @@
 import React from "react";
+import "../assets/styles/itemDetail.css";
 
 const ItemDetail = ({ item }) => {
+	//item.id, item.image, item.title, item.description, item.price
 	return (
-		<div className="card itemCard">
-			<img src={item.image} className="card-img-top w-50 itemImg" alt="..." />
-			<div className="card-body itemData">
-				<h5 className="card-title">{item.title}</h5>
-				<h5 className="card-text">{item.description}</h5>
-				<p className="card-text">$ {item.price}</p>
+		<div className="itemDetail">
+			<div id="carouselExampleDark" className="carousel carousel-dark slide itemCarousel">
+				<div className="carousel-indicators">
+					<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+					<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+					<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+				</div>
+				<div className="carousel-inner">
+					<div className="carousel-item active" data-bs-interval="10000">
+						<img src={item.image} className="d-block itemCarouselImg" alt="..." />
+					</div>
+					<div className="carousel-item" data-bs-interval="2000">
+						<img src={item.image} className="d-block itemCarouselImg" alt="..." />
+					</div>
+					<div className="carousel-item">
+						<img src={item.image} className="d-block itemCarouselImg" alt="..." />
+					</div>
+				</div>
+				<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+					<span className="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span className="visually-hidden">Previous</span>
+				</button>
+				<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+					<span className="carousel-control-next-icon" aria-hidden="true"></span>
+					<span className="visually-hidden">Next</span>
+				</button>
+			</div>
+			<div className="itemInfo">
+				<h1>{item.title}</h1>
+				<h4>{item.description}</h4>
+				<div className="itemInfoBuy">
+					<h2>$ {item.price}</h2>
+					<button type="button" class="btn btn-dark">
+						Agregar al carrito
+					</button>
+				</div>
 			</div>
 		</div>
 	);
